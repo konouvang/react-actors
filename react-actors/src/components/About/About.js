7788e6393f2bdf46ea60ad4853de1f4fd0eacdf7
Konou Vang <konouvang@Konous-MBP.attlocal.net>
2019-05-07 18:16:05 -0500

    all done

---
+++ react-actors/src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>Until 1993 collect calling was a virtual monopoly held by AT&#38T as people were accustomed to dialing "0" to place collect calls. MCI moved aggressively to insert itself into the market by launching 1-800-COLLECT that year. By dialing 1-800-COLLECT, customers could connect with an automated MCI system which would directly place a call to a designated receiving party for a fraction of the cost of the AT&#38T service for its operator-assisted collect calling. According to Advertising Age, 1-800-COLLECT went from concept to launch in less than three months.[1] Launched with a large marketing budget, within a year New York Magazine reported that MCI had "stamped 1-800-COLLECT onto our consciousness with a mammoth marketing blitz" aimed largely at Generation X consumers</p>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ react-actors/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p>copyright © React Actors</p>
+            </div>
+
+        );
+    }
+}
+
+export default Footer;
